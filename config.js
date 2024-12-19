e864741cfdac15996f2d85f34596031d810c3e38
tchabal55 <atistchabal@gmail.com>
2024-12-19 15:44:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SESSION HERE",
+SESSION_ID: process.env.SESSION_ID || "SJQDGCqL#yehhdXoWmfEP4y4HrVATUXtFGIufNahczn2eyR5Wy_s",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
